277ac2b073b0be891b11d666e922ac58c0ba0ee8
aprilia anggun sari rahmawati <92620594+aprilianggun@users.noreply.github.com>
2022-06-15 09:00:43 +0700

    Add files via upload

---
+++ project uap petruk.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<list>
+#include<iterator>
+using namespace std;
+
+list<int>Kereta;
+int temp;
+
+void Push(){
+	cout<<"masukan nomor antrian: ";
+	cin>>temp;
+	Kereta.push_back(temp);
+	
+}
+
+int main(){
+	
+	int p;
+	
+	do{
+		
+		
+	}
+	
+}
